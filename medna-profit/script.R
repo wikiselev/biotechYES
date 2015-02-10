78aa175bd99e9fcc47a099cd17b6ec613663ba67
wikiselev <vladimir.kiselev@babraham.ac.uk>
2015-02-10 21:42:06 +0000

    removed test from repository

--- medna-profit/script.R
+++ medna-profit/script.R
@@ -1,6 +1,9 @@
 library(ggplot2)
 
-d <- read.table("medna-profit/net-profit-accumulative.txt")
-p <- ggplot(d, aes(V1, V2)) + geom_area(aes(fill = "red")) + theme_bw() + labs(x = "Years", y = "Pounds")
-p
-ggsave("net-profit-acc.pdf")
+d <- read.table("net-profit-accumulative.txt")
+p <- ggplot(d, aes(V1, V2)) +
+	geom_line(aes(color=V3)) +
+	theme_bw() +
+	# facet_wrap( ~ V3) +
+	labs(x = "Years", y = "Pounds") +
+ggsave("net-profit-acc.pdf", w = 8, h = 4)
